8cfa1a11449ccf852a17fce5fabb887de8a4f432
JiaLee0707 <s2018s37@e-mirim.hs.kr>
2019-03-26 19:47:12 +0900

    add Echo project

--- 20190326_Echo/src/Echo.java
+++ 20190326_Echo/src/Echo.java
@@ -1,4 +1,18 @@
+import java.util.Scanner;
 
 public class Echo {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("자신감 갖기");
+		while(true) {
+			System.out.print("메아리 : ");
+			String inputString = sc.nextLine();
+			if(inputString.equals("exit")) {
+				break;
+			}
+			System.out.println(inputString);
+		}
+	}
 
 }
